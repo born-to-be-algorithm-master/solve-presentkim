ca9adaf4518818eb0ce3c0ce08743971da7b2781
PresentKim <debe3721@gmail.com>
2024-05-14 00:35:56 +0900

    :recycle: Refactoring kotlin solution of `프로그래머스 250136 - [PCCP 기출문제] 2번 / 석유 시추`

--- src/main/java/kim/present/solve/programmers/Solution250136kt.kt
+++ src/main/java/kim/present/solve/programmers/Solution250136kt.kt
@@ -5,54 +5,42 @@ package kim.present.solve.programmers
  */
 class Solution250136kt {
     fun solution(land: Array<IntArray>): Int {
-        val n = land.size // n : 땅의 세로길이
-        val m = land[0].size // m : 땅의 가로길이
+        var oilCount = 0 // 석유 덩어리 인덱스
+        // 석유 덩어리별 매장량을 계산하고 배열에 저장
+        val oils = land.flatMapIndexed { y, line -> line.mapIndexed { x, value -> Triple(x, y, value) } }
+            .mapNotNull { (x, y, value) -> if (value == 1) groupingOil(land, x, y, oilCount++) else null }
 
-        val oils: MutableList<Int> = ArrayList() // 석유 덩어리의 크기를 저장하는 리스트
-        for (x in 0 until m) {
-            for (y in 0 until n) {
-                if (land[y][x] == 1) oils.add(replaceConnectedOil(land, x, y, -oils.size - 1))
+        // 땅의 가로 길이만큼 순회하며 시추 가능한 석유량 계산 후 최대값 반환
+        return (0 until land[0].size).maxOf { x ->
+            val visited = BooleanArray(oilCount) // 석유 덩어리 방문 여부를 저장할 배열
+            land.map { -it[x] - 1 }.sumOf { oilIndex -> // 석유 덩어리 인덱스로 변환 후 순회 결과 합산
+                if (oilIndex != -1 && !visited[oilIndex]) { // 방문하지 않은 석유 덩어리인 경우
+                    visited[oilIndex] = true // 방문 처리
+                    oils[oilIndex] // 석유 덩어리의 매장량 반환
+                } else 0 // 시추 불가능한 땅은 0 반환
             }
         }
-
-        var max = 0
-        for (x in 0 until m) {
-            var amount = 0
-            val visited = BooleanArray(oils.size)
-            land.forEachIndexed { y, line ->
-                val value = line[x]
-                val oilIndex = -value - 1
-                if (value != 0 && !visited[oilIndex]) {
-                    amount += oils[oilIndex]
-                    visited[oilIndex] = true
-                }
-            }
-            if (amount > max) max = amount // 최대값 갱신
-        }
-
-        return max // 최대값 반환
     }
 
-    private fun replaceConnectedOil(land: Array<IntArray>, startX: Int, startY: Int, value: Int): Int {
-        var count = 0
-        val stack = ArrayDeque<Pair<Int, Int>>().apply { add(startX to startY) }
+    private fun groupingOil(land: Array<IntArray>, startX: Int, startY: Int, oilIndex: Int): Int {
+        var count = 0 // 석유 덩어리의 매장량
+        val queue = ArrayDeque(listOf(startX to startY)) // BFS 처리를 위한 큐
 
-        while (!stack.isEmpty()) {
-            val (x, y) = stack.removeLast()
+        while (!queue.isEmpty()) { // 큐가 비어있을 때까지 반복
+            val (x, y) = queue.removeLast()
 
+            // 땅의 범위를 벗어나거나 이미 방문한 땅인 경우 무시
             if (x < 0 || x >= land[0].size || y < 0 || y >= land.size || land[y][x] != 1) {
                 continue
             }
 
-            land[y][x] = value
-            count++
+            land[y][x] = -oilIndex - 1 // 땅의 값을 석유 덩어리 인덱스를 음수로 변환한 값으로 변경 (방문 처리, 기본값인 0과 1과 다른 값을 넣기 위해)
+            count++ // 석유 덩어리의 매장량 증가
 
-            for (i in -1..1 step 2) {
-                stack.add(x + i to y)
-                stack.add(x to y + i)
-            }
+            // 상하좌우 4개의 좌표를 큐에 추가
+            queue.addAll((-1..1 step 2).flatMap { listOf(x + it to y, x to y + it) })
         }
 
-        return count
+        return count // 석유 덩어리의 매장량 반환
     }
 }
